e71e159d86c48d363a29d2c0e501ff1cdac4bcca
Nazme10 <157196884+Nazme10@users.noreply.github.com>
2025-08-08 01:06:21 -0500

    ...

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ class Main:
         self.clock = pygame.time.Clock()
         self.ship = Ship(self.screen)
 
+         #movement flags 
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
         # Font for FPS
         self.font = pygame.font.SysFont("Arial", 24)
 
@@ -25,21 +31,87 @@ class Main:
         #y=100
        
         while True:
-            events = pygame.event.get()
-            print(events)
-            for event in events:
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    return
+            self.check_event()
+            self.render()
+
             self.screen.fill(self.settings.bg_color)
-            self.render_fps(self.screen, self.clock, self.font)
+            #self.render_fps(self.screen, self.clock, self.font)
             self.ship.blitme()
-            pygame.draw.rect(self.screen, (72, 61, 139), (100,200,200,400))
+            #pygame.draw.rect(self.screen, (72, 61, 139), (100,200,200,400))
             #x+=.1
             #y+=.2
             
             pygame.display.flip()
             self.clock.tick()
+
+    def check_event(self):
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT: 
+                pygame.quit()
+                return
+            
+        #flagging
+        
+            elif event.type == pygame.KEYDOWN: 
+                if event.key == pygame.K_RIGHT:
+                    self.moving_right = True
+
+                elif event.key == pygame.K_LEFT:
+                    self.moving_left = True
+
+                elif event.key == pygame.K_UP:
+                    self.moving_up = True
+
+                elif event.key == pygame.K_DOWN:
+                    self.moving_down = True
+            
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.moving_right = False
+
+                elif event.key == pygame.K_LEFT:
+                    self.moving_left = False
+                    
+                elif event.key == pygame.K_UP:
+                    self.moving_up = False
+
+                elif event.key == pygame.K_DOWN:
+                    self.moving_down = False
+
+            
+        #keys = pygame.key.get_pressed()
+        # #if keys[pygame.K_UP]:
+        #     self.ship.ship_rect.y -= 2
+        # #if keys[pygame.K_DOWN]:
+        #     self.ship.ship_rect.y += 2
+        # #if keys[pygame.K_LEFT]:
+        #     self.ship.ship_rect.x -= 2
+        # #if keys[pygame.K_RIGHT]:
+        # ##    self.ship.ship_rect.x += 2
+
+    def render_ship(self):
+        if self.moving_right == True:
+                self.ship.ship_rect.x += 1
+        if self.moving_left == True:
+            self.ship.ship_rect.x -= 1
+        if self.moving_up == True:
+                self.ship.ship_rect.y -= 1
+        if self.moving_down == True:
+                self.ship.ship_rect.y += 1
+        self.ship.blitme()
+
+    def render(self):
+            self.screen.fill(self.settings.bg_color)
+            self.render_fps(self.screen, self.clock, self.font)
+
+            
+            #pygame.draw.rect(self.screen, (72, 61, 139), (100,200,200,400))
+            #x+=.1
+            #y+=.2
+            self.render_ship()
+            pygame.display.flip()
+            self.clock.tick()        
 
     def render_fps(self,screen, clock, font):
         fps = int(clock.get_fps())
